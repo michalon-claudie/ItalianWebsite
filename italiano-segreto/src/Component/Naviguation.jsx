95ec95e3d13480e163b8aaa441343a734f7b051d
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-07-16 19:44:09 +0200

    add styles on projects

--- italiano-segreto/src/Component/Naviguation.jsx
+++ italiano-segreto/src/Component/Naviguation.jsx
@@ -15,11 +15,8 @@ export default function Navigation() {
         <nav className="navigation" >
             <NavItem href="/" label="Home" />
             <NavItem href="lamiastoria.html" label="La mia storia" />
-            <NavItem href="Torino.html" label="Torino" />
             <NavItem href="/Firenze" label="Firenze" />
-            <NavItem href="Sicilia.html" label="La Sicilia" />
             <NavItem href="Ricetta.html" label="Ricette" />
-            <NavItem href="Toscana.html" label="Toscana" />
             <NavItem href="Imparare.html" label="Imparare l'Italiano" />
             <NavItem href="Contact.html" label="Contact" />
         </nav>
--- italiano-segreto/src/Pages/Firenze.jsx
+++ italiano-segreto/src/Pages/Firenze.jsx
@@ -3,6 +3,7 @@ import '../Style/style.css'
 import Header from '../Component/Header'
 import Banner from '../Component/Banner'
 import Naviguation from '../Component/Naviguation'
+import Files from '../Component/Files'
 import Footer from '../Component/Footer'
 
 export default function Firenze() {
@@ -11,6 +12,7 @@ export default function Firenze() {
             <Header/>
             <Banner text="Firenze"/>
             <Naviguation/>
+            <Files/>
             <Footer/>
         </main>
     )
