c34a2ba55484699855eab127ed46c090c583a678
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-08-12 09:59:04 +0200

    fix bug dotenv + require package

--- italiano-segreto/backend/db/connection.ts
+++ italiano-segreto/backend/db/connection.ts
@@ -1,1 +1,21 @@
-const mysql= require('mysql2');
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+connection.connect((error) => {
+  if (error) {
+      console.error('Error connecting to the database:', error);
+      return;
+  }
+  console.log('Connected to the database.');
+});
+
+export default connection;
