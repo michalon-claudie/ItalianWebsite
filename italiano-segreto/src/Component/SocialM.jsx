0947a40344eed72801caa21c0282b9b3a44a4c3d
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-09-03 10:16:28 +0200

    optimize footer code

--- italiano-segreto/src/Component/SocialM.jsx
+++ italiano-segreto/src/Component/SocialM.jsx
@@ -1,25 +1,50 @@
-import React from 'react'
-import imgFB from '../Images/fb.jpg'
-import insta from '../Images/insta.webp'
-import linkdin from '../Images/linkdIn.webp'
+import React from 'react';
+import imgFB from '../Images/fb.jpg';
+import insta from '../Images/insta.webp';
+import linkdin from '../Images/linkdIn.webp';
 
-export default function Social(){
-    return(
+export default function Social() {
+
+    const socialLinks = [
+        {
+            href: "https://www.instagram.com/ita.lie.per.tutti/?next=%2F",
+            imgSrc: imgFB,
+            alt: "Facebook",
+            ariaLabel: "link facebook"
+        },
+        {
+            href: "https://www.instagram.com/ita.lie.per.tutti/?next=%2F",
+            imgSrc: insta,
+            alt: "Instagram",
+            ariaLabel: "link instagram"
+        },
+        {
+            href: "https://www.linkedin.com/in/claudie-michalon-8b1a0422a/",
+            imgSrc: linkdin,
+            alt: "LinkedIn",
+            ariaLabel: "link LinkedIn"
+        }
+    ];
+
+    return (
         <div className='flex flex-row items-center justify-between m-5'>
             <div className="flex gap-px md:gap-4">
-                <a className= "w-3/12 md:w-[7%] transition-transform duration-500 hover:transform hover:-translate-y-1" href="https://www.instagram.com/ita.lie.per.tutti/?next=%2F" target="_blank" aria-label="lien vers facebook">
-                    <img className='w-full'src={imgFB} alt='Facebook'/>
-                </a>
-                <a className= "w-[4%] md:w-[7%] transition-transform duration-500 hover:transform hover:-translate-y-1" href="https://www.instagram.com/ita.lie.per.tutti/?next=%2F" target="_blank" aria-label="lien vers instagram">
-                    <img className='w-full'src={insta} alt='instagram'/>
-                </a>
-                <a className= "w-[4%] md:w-[7%] transition-transform duration-500 hover:transform hover:-translate-y-1" href="https://www.linkedin.com/in/claudie-michalon-8b1a0422a/" target="_blank" aria-label='lien vers LinkdIn'>
-                    <img className='w-full'src={linkdin} alt='linkdIn'/>
-                </a>
+                {socialLinks.map((link, index) => (
+                    <a
+                        key={index}
+                        className={`w-3/12 md:w-[7%] transition-transform duration-500 hover:transform hover:-translate-y-1`}
+                        href={link.href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label={link.ariaLabel}><img className='w-full' src={link.imgSrc} alt={link.alt} />
+                    </a>
+                ))}
             </div>
             <div className="mail">
-                <a className= "transition-transform duration-500 hover:transform hover:-translate-y-1" href="MAILTO:clodiedie@hotmail.fr" aria-label='Envoyer moi un mail'>Scrivimi</a>
+                <a className="transition-transform duration-500 hover:transform hover:-translate-y-1"
+                    href="MAILTO:clodiedie@hotmail.fr"
+                    aria-label='Envoyer moi un mail'>Scrivimi</a>
             </div>
         </div>
-    )
+    );
 }
