7396a79a52ea9e2fef1f35b5e807cd2a38f3d8f6
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-09-04 10:39:47 +0200

    add tailwind on footer

--- italiano-segreto/src/Component/Footer.jsx
+++ italiano-segreto/src/Component/Footer.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import '../Style/Footer.scss'
 import SocialM from '../Component/SocialM'
 
 export default function Footer(){
     return(
-    <footer>
-        <div className="footer_block">
-            <h2 className="titre-reseaux">Réseaux Sociaux</h2>
+    <footer className='flex flex-col justify-center items-center bg-customPurple'>
+        <div className="flex flex-col items-center text-center p-2.5 w-4/5">
+            <h2 className="text-4xl">Réseaux Sociaux</h2>
             <SocialM/>
         </div>
-        <div className="footer_block">
-            <h2 className="titre-monsite">Mon site</h2>
-            <a href="a-propos.html">À propos</a>
-            <a href="Contact.html">Contact</a>
+        <div className='flex flex-col justify-center items-center bg-customPurple'>
+            <h2 className="text-4xl">Mon site</h2>
+            <a className="text-2xl" href="a-propos.html">À propos</a>
+            <a className="text-2xl" href="Contact.html">Contact</a>
         </div>
     </footer>
     )
--- italiano-segreto/tailwind.config.js
+++ italiano-segreto/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        customPurple: '#ebceeb',
+      },
       fontFamily: {
         parisienne: ['Parisienne', 'cursive'],
       },
