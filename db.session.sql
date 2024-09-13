23c48562643b0e67ba87731b0913446cf5c6b4f8
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-09-13 14:36:28 +0200

    create articles table

--- db.session.sql
+++ db.session.sql
@@ -1,4 +1,6 @@
-INSERT INTO pages (id,name)VALUES (
-    '2',
-    'Rome'
+CREATE TABLE articles(
+    id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(100),
+    picture VARCHAR(400),
+    text VARCHAR(900)
 );
--- italiano-segreto/backend/migrations/pages.sql
+++ italiano-segreto/backend/migrations/pages.sql
@@ -2,3 +2,10 @@ CREATE TABLE pages (
     id INT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE articles(
+    id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(100),
+    picture VARCHAR(400),
+    text VARCHAR(900)
+);
