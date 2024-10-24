34317167eaee28e80442af241e2d3779c45a53a5
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-10-24 16:55:24 +0200

    fix error index and add fix error files.jsx style

--- italiano-segreto/src/Component/Files.jsx
+++ italiano-segreto/src/Component/Files.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 
 export default function Files(){
     return(
-        <section className='flex flex-wrap gap-8 justify-center my-12 p-5'>
+        <section className='flex flex-wrap gap-16 justify-center my-12 p-5'>
             {files.map(completeFiles => (
-                    <div key={completeFiles.id} className='w-full transition-transform duration-1000 relative md:w-2/5 hover:scale-110 hover:brightness-50'>
+                    <div key={completeFiles.id} className='w-full transition-transform duration-1000 relative md:w-4/12 hover:scale-110 hover:brightness-50'>
                         <Link className="flex w-full" to={`/${completeFiles.title}`}>
                             <img className='w-full h-72' src={completeFiles.cover} alt={completeFiles.title}/>
                             <span className='absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 text-white p-1'>{completeFiles.title}</span>
